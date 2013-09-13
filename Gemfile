c907445fb858db5a82ef40db9762b9e0f179ffcd
Jed Foster <jed@jedfoster.com>
2013-09-13 07:40:23 -0700

    First commit. Works OK-ish

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+ruby '1.9.3'
+
+gem 'sinatra'
+gem 'unicorn'
+gem 'newrelic_rpm'
+
+group :development do
+  gem 'activesupport', '~> 4.0.0'
+end
+
+gem 'html-pipeline', '~> 0.2.1'
+gem 'haml'
+gem 'RedCloth'
+gem 'slim'
+
+# gem 'pry-remote'
---
+++ app.rb
@@ -0,0 +1,88 @@
+$LOAD_PATH.unshift(File.join(File.dirname(File.realpath(__FILE__)), 'lib'))
+
+require 'rubygems'
+
+# If you're using bundler, you will need to add this
+require 'bundler/setup'
+
+require 'sinatra/base'
+require 'json'
+
+require 'html/pipeline'
+require 'RedCloth'
+require 'haml'
+require 'slim'
+
+require './lib/html/pipeline/haml.rb'
+
+# require 'pry-remote'
+
+class SassMeisterCompilerApp < Sinatra::Base
+  configure :production do
+    APP_HOST = 'sassmeister.com'
+    require 'newrelic_rpm'
+  end
+
+  configure :development do
+    APP_HOST = 'sassmeister.dev'
+  end
+
+
+  helpers do
+    def render_html(html, filter)
+      context = {
+        :gfm => true,
+        :whitelist => HTML::Pipeline::SanitizationFilter::WHITELIST
+      }
+
+      if filter == 'Textile'
+        filter = HTML::Pipeline::TextileFilter
+
+      elsif filter == 'Haml'
+        filter = HTML::Pipeline::HamlFilter
+
+      else
+        filter = HTML::Pipeline::MarkdownFilter
+      end
+
+      pipe = HTML::Pipeline.new [
+        filter
+
+      ], context
+
+      pipe.call(html)[:output]
+    end
+  end
+
+
+  before do
+    params[:syntax].downcase! unless params[:syntax].nil?
+    params[:original_syntax].downcase! unless params[:original_syntax].nil?
+    params[:html_syntax].downcase! unless params[:html_syntax].nil?
+
+    headers 'Access-Control-Allow-Origin' => "http://#{APP_HOST}"
+  end
+
+  get '/' do
+    status 501
+  end
+
+
+  post '/' do
+    case params[:html_syntax]
+    when 'haml'
+        return render_html(params[:html], 'Haml')
+
+    when 'markdown'
+        return render_html(params[:html], 'Markdown')
+
+    when 'textile'
+        return render_html(params[:html], 'Textile')
+
+    else
+      return params[:html]
+    end
+  end
+
+  run! if app_file == $0
+end
