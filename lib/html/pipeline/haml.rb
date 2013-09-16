92e284e26a87f569b085a90efa9bd977b216aa42
Jed Foster <jed@jedfoster.com>
2013-09-15 18:18:33 -0700

    Handle NameErrors

--- lib/html/pipeline/haml.rb
+++ lib/html/pipeline/haml.rb
@@ -12,11 +12,15 @@ module HTML
 
           html.rstrip!
           html
+
         rescue Haml::Error => e
           '<span style="background: #B4004C; color: #FFFFFF; font: 120%/1.5 \'Helvetica Neue\', Arial, sans-serif; font-weight: 200; text-shadow: 1px 1px 0 #666666; padding: 0.5em 1em;">Haml error</span>'
 
         rescue Haml::SyntaxError => e
           '<span style="background: #B4004C; color: #FFFFFF; font: 120%/1.5 \'Helvetica Neue\', Arial, sans-serif; font-weight: 200; text-shadow: 1px 1px 0 #666666; padding: 0.5em 1em;">Haml syntax error</span>'
+
+        rescue NameError => e
+          "<span style=\"background: #B4004C; color: #FFFFFF; font: 120%/1.5 'Helvetica Neue', Arial, sans-serif; font-weight: 200; text-shadow: 1px 1px 0 #666666; padding: 0.5em 1em;\">#{html_escape e}</span>"
         end
       end
     end
