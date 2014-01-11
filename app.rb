fdb8826bb674784011ed4916a887f4e7e498cead
Jed Foster <jed@jedfoster.com>
2014-01-11 07:52:39 -0800

    Fix the origin regex so xip.io is actually detected

--- app.rb
+++ app.rb
@@ -67,7 +67,7 @@ class HtmlCompilerApp < Sinatra::Base
     def origin_allowed?(uri)
       return false if uri.nil?
 
-      return uri.match(/^http:\/\/(.+\.){0,1}sassmeister\.(com|dev|([\d+\.]{4}xip\.io))/)
+      return uri.match(/^http:\/\/(.+\.){0,1}sassmeister\.(com|dev|((\d+\.){4}xip\.io))/)
     end
   end
 
